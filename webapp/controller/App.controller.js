3e4f0fb25e6b2c92df21666e5aebee12fca6a334
skronschlaeger16 <sebkro90@gmail.com>
2022-02-18 16:45:33 +0100

    Dialogs+Fragments

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,22 +1,13 @@
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
+
   ],
-  function (Controller, MessageToast, JSONModel, ResourceModel) {
+  function (Controller) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-      onShowHello: function () {
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        MessageToast.show(sMsg);
-      },
+    
     });
   }
 );
