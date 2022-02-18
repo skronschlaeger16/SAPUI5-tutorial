4939af7c6b546e420216d0a70c855ea9cdf8b96c
skronschlaeger16 <sebkro90@gmail.com>
2022-02-18 14:56:23 +0100

    further steps

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,22 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
-    "use strict"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+  ],
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-        onShowHello: function () {
-
-        MessageToast.show("Hello there!")      
-      }
+      onShowHello: function () {
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        MessageToast.show(sMsg);
+      },
     });
-})
+  }
+);
